e6a5111e1d5d2ef9b896c3624b568dc7bed7c404
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2024-10-26 09:37:06 -0300

    activity CA

--- src/app/dashboard/customers/page.tsx
+++ src/app/dashboard/customers/page.tsx
@@ -1,18 +1,9 @@
-import CustomerTable from "@/layers/customers/components/customers-table";
-import { getCustomers } from "@/lib/actions";
-import React from "react";
+import React, { Suspense } from "react";
 
 export default async function CustomersPages() {
-  const customers = await getCustomers();
   return (
-    <div>
-      <div className="flex justify-between items-center p-2">
-        <h2 className="font-bold">Clientes</h2>
-      </div>
-      <hr />
-      <div className="flex flex-col gap-0 p-4 max-md:p-1">
-        <CustomerTable customers={customers} />
-      </div>
-    </div>
+    <Suspense fallback="Loading...">
+      <CustomersPages />
+    </Suspense>
   );
 }
