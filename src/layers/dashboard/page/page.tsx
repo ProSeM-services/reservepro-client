40af384fa5cc9bf191c5f6cb9c0060debfd5c4f7
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2024-11-09 17:41:11 -0300

    fix styles

--- src/layers/dashboard/page/page.tsx
+++ src/layers/dashboard/page/page.tsx
@@ -10,22 +10,23 @@ export function DashboardPage() {
   return (
     <div className=" h-[100%] flex flex-col  gap-4  ">
       <section className="flex flex-col  gap-4  h-full">
-        <div className="flex  gap-4  max-lg:flex-col  h-1/2  ">
+        <div className="flex  gap-4  max-lg:flex-col  md:h-1/2  ">
           <DashboardCard type="company" />
           <DashboardCard type="member" />
         </div>
 
-        <section className="flex gap-4 h-1/2 ">
-          <div className=" h-1/3  space-y-2 w-1/2   ">
+        <section className="flex max-md:flex-col gap-4 ,md:h-1/2 ">
+          <div className=" h-1/3  space-y-2 md:w-1/2   ">
             <div className="flex items-center justify-between">
               <h2 className="font-semibold">Servicios</h2>
               <AddButton type="services" />
             </div>
             <hr />
-
-            <ServicesList />
+            <div className="max-md:max-h-full max-md:overflow-auto">
+              <ServicesList />
+            </div>
           </div>
-          <div className=" h-full w-1/2 grid place-items-center">
+          <div className=" h-full md:w-1/2 grid place-items-center">
             <CustomerStats />
           </div>
         </section>
